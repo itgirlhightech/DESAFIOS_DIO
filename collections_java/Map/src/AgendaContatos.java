344636201c599ceda042a1e8d6d1ef228fedf4ed
Evilyn Feitosa <evy123feitosa@gmail.com>
2024-05-24 17:03:09 -0300

    desafio DIO

---
+++ collections_java/Map/src/AgendaContatos.java
@@ -0,0 +1,46 @@
+import java.util.HashMap;
+import java.util.Map;
+
+public class AgendaContatos {
+
+    private Map<String, Integer> agendaContatoMap;
+
+    public AgendaContatos() {
+        this.agendaContatoMap = new HashMap<>();
+
+
+    }
+    public void adicionarContato(String nome, Integer telefone) {
+        agendaContatoMap.put(nome, telefone);
+    }
+
+    public void removerContato(String nome) {
+        if(!agendaContatoMap.isEmpty()) {
+            agendaContatoMap.remove(nome);
+        }
+    }
+    public void exibirContato() {
+        System.out.println(agendaContatoMap);
+    }
+
+    public Integer pesquisarPorNome(String nome) {
+        Integer numeroPorNome = null;
+        if(!agendaContatoMap.isEmpty()) {
+            numeroPorNome = agendaContatoMap.get(nome);
+        }
+     return numeroPorNome;
+    }
+
+    public static void main(String[] args) {
+        AgendaContatos agendaContatos = new AgendaContatos();
+
+        agendaContatos.adicionarContato("Lola", 9278262);
+        agendaContatos.adicionarContato("Lila", 9279282);
+        agendaContatos.adicionarContato("Louise", 93678262);
+
+        agendaContatos.exibirContato();
+
+        agendaContatos.removerContato("Louise");
+        agendaContatos.exibirContato();
+    }
+}
